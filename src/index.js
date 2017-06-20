09b905a388aaf691fc9eb6c2a95e3e7107b11284
Max Orelus <me@maxorelus.com>
2017-06-20 14:05:31 -0400

    original default

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 require('dotenv').config();
-import fs from 'fs';
-import https from 'https';
-const privateKey = fs.readFileSync('server.key', 'utf8');
-const certificate = fs.readFileSync('server.crt', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
 import express from 'express';
 import { middlewareSetup } from './middleware';
 import {
@@ -17,8 +11,6 @@ const app = express();
 middlewareSetup(app);
 app.use('/v1', [searchRoutes, getRoutes, getSourceRoutes]);
 
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(~~process.env.PORT, () => {
+app.listen(~~process.env.PORT, () => {
   console.log(`CDP service listening on port: ${process.env.PORT}`);
 });
