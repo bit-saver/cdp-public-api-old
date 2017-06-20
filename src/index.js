0a3b24aed231d089ef7e416bd1acdc9bc71f4a2e
Max Orelus <me@maxorelus.com>
2017-06-20 14:13:53 -0400

    original version

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ import {
 } from './modules';
 
 const app = express();
+
 middlewareSetup(app);
-app.use('/v1', [searchRoutes, getRoutes, getSourceRoutes]);
+
+app.use('/v1', [
+  searchRoutes,
+  getRoutes,
+  getSourceRoutes
+]);
 
 app.listen(~~process.env.PORT, () => {
   console.log(`CDP service listening on port: ${process.env.PORT}`);
