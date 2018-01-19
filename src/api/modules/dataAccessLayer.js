5e91f7ebe58e5a85a0c1a5b682a3692bd8f8f6fb
Tyler Hebenstreit <tyler.hebenstreit@Tyler-Hebenstreits-MacBook-Pro.local>
2018-01-19 15:27:32 -0800

    API structure change proposal: All code related directly to ElasticSearch has been moved into modules/elastic. Parser and Query are still separated, but combined in elastic/controller.js. This allows each to be tested individually.
    modles/query.js has been renamed to dataAccessLayer since it acts as the generic medium between the API
    and the backend database.
    This allows us to remain database agnostic by simply switching the DAL to use a different database module (elastic is currently the only one).
    
    Database Access Layer can be called anything I just though this was logically appropriate.

--- src/api/modules/dataAccessLayer.js
+++ src/api/modules/dataAccessLayer.js
@@ -1,26 +1,4 @@
-import esParser from './esParser';
-
-// Pull controllers out to properly test
-// It will make it easier to integrate a db if we decide to do so
-export const controllers = {
-  indexDocument( model, docToIndex ) {
-    return model.indexDocument( docToIndex ).then( esParser.parseCreateResult( docToIndex ) );
-  },
-
-  updateDocument( model, docToUpdate, data ) {
-    return model
-      .updateDocument( docToUpdate, data )
-      .then( esParser.parseUpdateResult( docToUpdate, data ) );
-  },
-
-  deleteDocument( model, docToDelete ) {
-    return model.deleteDocument( docToDelete ).then( esParser.parseDeleteResult( docToDelete ) );
-  },
-
-  getDocument( model, docToGet ) {
-    return model.getDocument( docToGet ).then( esParser.parseGetResult( docToGet ) );
-  }
-};
+import controllers from './elastic/controller';
 
 // POST v1/[resource]
 export const indexDocument = model => ( req, res, next ) => {
