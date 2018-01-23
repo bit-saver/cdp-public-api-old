5dcb6910092f18657d72e014c3f573b0f9fe01fb
Tyler Hebenstreit <tyler.hebenstreit@Tyler-Hebenstreits-MacBook-Pro.local>
2018-01-23 10:10:56 -0800

    Updated tests to use more contemporary async/await style.  Also fixed some issues (namely delete erroneously using update functions).

--- src/api/modules/elastic/parser.test.js
+++ src/api/modules/elastic/parser.test.js
@@ -5,26 +5,23 @@ describe.only( 'Modules.elastic', () => {
   describe( 'parser', () => {
     const testID = 1;
     let testObj;
+    let result;
     describe( 'parseCreateRequest', () => {
       testObj = { body: 'test body' };
       const succResult = { _id: '1', found: true };
       it( 'should be a function', () => {
         expect( parser.parseCreateResult ).to.be.a( 'function' );
       } );
-      it( 'should return an id and the provided body', () => {
-        parser
-          .parseCreateResult( testObj )( { ...succResult, result: 'created' } )
-          .then( ( result ) => {
-            expect( result ).to.deep.equals( { id: '1', ...testObj } );
-          } )
-          .catch( ( err ) => {} );
+      it( 'should return an id and the provided body', async () => {
+        result = await parser.parseCreateResult( testObj )( {
+          ...succResult,
+          result: 'created'
+        } );
+        expect( result ).to.deep.equals( { id: '1', ...testObj } );
       } );
-      it( 'should reject with input if not created', () => {
-        parser
+      it( 'should reject with input if not created', async () => {
+        await parser
           .parseCreateResult( testObj )( { ...succResult, result: 'created' } )
-          .then( ( result ) => {
-            // noop failure
-          } )
           .catch( ( err ) => {
             expect( err ).to.deep.equals( testObj );
           } );
@@ -35,20 +32,17 @@ describe.only( 'Modules.elastic', () => {
       it( 'should be a function', () => {
         expect( parser.parseGetResult ).to.be.a( 'function' );
       } );
-      it( 'should return a formatted object', () => {
-        parser
-          .parseGetResult( testID )( { _id: '1', _source: testObj, found: true } )
-          .then( ( result ) => {
-            expect( result ).to.deep.equals( { ...testObj, id: '1' } );
-          } )
-          .catch( ( err ) => {} );
+      it( 'should return a formatted object', async () => {
+        result = await parser.parseGetResult( testID )( {
+          _id: '1',
+          _source: testObj,
+          found: true
+        } );
+        expect( result ).to.deep.equals( { ...testObj, id: '1' } );
       } );
-      it( 'should reject with input if not found', () => {
-        parser
+      it( 'should reject with input if not found', async () => {
+        await parser
           .parseGetResult( testID )( { found: false } )
-          .then( ( result ) => {
-            // noop failure
-          } )
           .catch( ( err ) => {
             expect( err ).to.equal( testID );
           } );
@@ -59,51 +53,41 @@ describe.only( 'Modules.elastic', () => {
       it( 'should be a function', () => {
         expect( parser.parseUpdateResult ).to.be.a( 'function' );
       } );
-      it( 'should reject with the original input if not found', () => {
-        parser
+      it( 'should reject with the original input if not found', async () => {
+        await parser
           .parseUpdateResult( testID, testObj )( { found: false } )
-          .then( ( result ) => {
-            // noop failure
-          } )
           .catch( ( err ) => {
             expect( err ).to.equal( testID );
           } );
       } );
-      it( 'should return the updated doc if updated', () => {
-        parser
-          .parseUpdateResult( testID, testObj )( { _id: 1, result: 'updated', found: true } )
-          .then( ( result ) => {
-            expect( result ).to.equal( { id: `${testID}`, ...testObj } );
-          } )
-          .catch( ( err ) => {
-            // noop failure
-          } );
+      it( 'should return the updated doc if updated', async () => {
+        result = await parser.parseUpdateResult( testID, testObj )( {
+          _id: '1',
+          result: 'updated',
+          found: true
+        } );
+        expect( result ).to.deep.equal( { id: `${testID}`, ...testObj } );
       } );
     } );
     describe( 'parseDeleteResult', () => {
       testObj = { body: 'test body2' };
       it( 'should be a function', () => {
-        expect( parser.parseUpdateResult ).to.be.a( 'function' );
+        expect( parser.parseDeleteResult ).to.be.a( 'function' );
       } );
-      it( 'should reject with the original input if not found', () => {
-        parser
-          .parseUpdateResult( testID, testObj )( { found: false } )
-          .then( ( result ) => {
-            // noop failure
-          } )
+      it( 'should reject with the original input if not found', async () => {
+        await parser
+          .parseDeleteResult( testID, testObj )( { found: false } )
           .catch( ( err ) => {
             expect( err ).to.equal( testID );
           } );
       } );
-      it( 'should return the deleted ID as success', () => {
-        parser
-          .parseUpdateResult( testID, testObj )( { _id: 1, result: 'deleted', found: false } )
-          .then( ( result ) => {
-            expect( result ).to.equal( `${testID}` );
-          } )
-          .catch( ( err ) => {
-            // noop failure
-          } );
+      it( 'should return the deleted ID as success', async () => {
+        result = await parser.parseDeleteResult( testID, testObj )( {
+          _id: '1',
+          result: 'deleted',
+          found: false
+        } );
+        expect( result ).to.equal( testID );
       } );
     } );
   } );
