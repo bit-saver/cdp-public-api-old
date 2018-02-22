5fdc3c35e51c2200d173d9624c796c876e5a7dce
Tyler Hebenstreit <tyler.hebenstreit@Tyler-Hebenstreits-MacBook-Pro.local>
2018-02-22 13:07:58 -0800

    CDP-487: Create Course routes, endpoints and models
    Create basic functional structures for the course endpoint.

--- src/api/resources/course/routes.js
+++ src/api/resources/course/routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import controller from './controller';
-import PostModel from './postModel';
+import CourseModel from './courseModel';
 import { transferCtrl, deleteCtrl } from '../../../middleware/transfer';
 import asyncResponse from '../../../middleware/asyncResponse';
 
@@ -8,14 +8,14 @@ const router = new Router();
 
 router.param( 'uuid', controller.setRequestDoc );
 
-// Route: /v1/post
-router.route( '/' ).post( asyncResponse, transferCtrl( PostModel ), controller.indexDocument );
+// Route: /v1/course
+router.route( '/' ).post( asyncResponse, transferCtrl( CourseModel ), controller.indexDocument );
 
-// Route: /v1/post/[uuid]
+// Route: /v1/course/[uuid]
 router
   .route( '/:uuid' )
-  .put( asyncResponse, transferCtrl( PostModel ), controller.updateDocumentById )
+  .put( asyncResponse, transferCtrl( CourseModel ), controller.updateDocumentById )
   .get( controller.getDocumentById )
-  .delete( deleteCtrl( PostModel ), controller.deleteDocumentById );
+  .delete( deleteCtrl( CourseModel ), controller.deleteDocumentById );
 
 export default router;
