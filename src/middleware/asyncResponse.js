a0b7c1f38cdb225d58a8ac1a6fd05bbc37b5fbc3
Terri Regan <contact@terriregan.com>
2018-02-22 14:19:53 -0500

    Added check for PUT request in async function

--- src/middleware/asyncResponse.js
+++ src/middleware/asyncResponse.js
@@ -2,6 +2,11 @@
  *  and handle any errors in a separate callback further down the line.
  */
 const asyncResponse = ( req, res, next ) => {
+  // if this is a PUT, we need to operate on an existing doc
+  // return if one does not exist.
+  if ( req.method === 'PUT' && !req.esDoc ) {
+    return next( new Error( `Document not found with UUID: ${req.params.uuid}` ) );
+  }
   if ( req.headers.callback ) {
     console.log( 'sending async response' );
     res.json( { message: 'Sync in progress.', req: req.body } );
