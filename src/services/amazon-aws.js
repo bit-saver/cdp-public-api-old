06206135b701b9642f4276c9f20eb3e173e3a0e4
Terri Regan <contact@terriregan.com>
2018-02-26 16:08:14 -0500

    Update the envvars in code

--- src/services/amazon-aws.js
+++ src/services/amazon-aws.js
@@ -3,9 +3,9 @@ import AWS from 'aws-sdk';
 import URL from 'url';
 
 AWS.config.update( {
-  accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_S3_SECRET_KEY,
-  region: process.env.AWS_S3_REGION
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
 } );
 
 const s3 = new AWS.S3();
