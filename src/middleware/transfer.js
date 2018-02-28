9f0070963828125776ceed23a6d1ed136ccf6e6b
Terri Regan <contact@terriregan.com>
2018-02-28 15:11:34 -0500

    Added debuggined

--- src/middleware/transfer.js
+++ src/middleware/transfer.js
@@ -46,6 +46,7 @@ const deleteAssets = ( assets ) => {
 const isTypeAllowed = async ( url ) => {
   const allowedTypes = utils.getContentTypes();
   const contentType = await utils.getTypeFromUrl( url );
+  console.log( ` -----  CONTENT TYPE -----${contentType}` );
   if ( !contentType ) return false;
   return allowedTypes.includes( contentType );
 };
